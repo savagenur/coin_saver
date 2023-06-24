c7f296a2c8e11d7f2903352cac05a48d09a7c2f6
Nurbolot Muratbekuulu <savagenurkg@gmail.com>
2023-06-24 16:14:48 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/savagenur/coin_saver/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.savagenur.coin_saver
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: FlexThemeData.light(scheme: FlexScheme.brandBlue),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Home',
+        ),
+      ),
+    );
+  }
+}
